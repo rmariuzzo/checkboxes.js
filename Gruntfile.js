d2706583a4619ca45e92d888fb1005ba2aadab3c
Rubens Mariuzzo <rubens@mariuzzo.com>
2013-06-17 12:00:54 -0400

    jQuery dependency handled with Bower package manager.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,29 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> - <%= pkg.author %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/checkbox.js',
-        dest: 'build/checkbox.min.js'
-      }
-    }
-  });
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> - <%= pkg.author %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/checkbox.js',
+                dest: 'build/checkbox.min.js'
+            }
+        },
+        bower: {
+            install: {
+                //just run 'grunt bower:install' and you'll see files from your Bower packages in lib directory
+            }
+        }
+    });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-bower-task');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
 
 };
