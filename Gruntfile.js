2a6d01a21dcee266c3a0c3ad1fa00761414624f9
Rubens Mariuzzo <rubens@mariuzzo.com>
2013-06-17 11:43:26 -0400

    Initial project structure.

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> - <%= pkg.author %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/checkbox.js',
+        dest: 'build/checkbox.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
---
+++ src/checkbox.js
@@ -0,0 +1,20 @@
+/*! Checkbox.js - MIT - Rubens Mariuzzo */
+!function(doc) {
+    
+    var Checkbox = function(context) {
+        this.context = context;
+    }
+    
+    Checkbox.prototype.checkAll = function() {
+        
+    };
+    
+    Checkbox.prototype.uncheckAll = function() {
+        
+    };
+    
+    Checkbox.prototype.toggleAll = function() {
+        
+    };
+    
+}(window);
