ad484e134530958791c8489a0fff7e8d37fc6d66
Rubens Mariuzzo <rubens@mariuzzo.com>
2014-06-04 18:10:42 -0400

    Removed grunt-bower task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,9 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
+
         pkg: grunt.file.readJSON('package.json'),
+
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= pkg.version %> | <%= pkg.author %> | <%= grunt.template.today("yyyy-mm-dd") %> | <%= pkg.license %> */\n'
@@ -10,14 +12,10 @@ module.exports = function(grunt) {
                 src: 'src/jquery.checkboxes.js',
                 dest: 'build/jquery.checkboxes.min.js'
             }
-        },
-        bower: {
-            install: {}
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-bower-task');
 
     grunt.registerTask('default', ['uglify']);
 
