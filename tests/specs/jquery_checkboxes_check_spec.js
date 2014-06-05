0f52237b89bd53b1c3a231daa552b158a65ffa82
Rubens Mariuzzo <rubens@mariuzzo.com>
2014-06-04 22:57:56 -0400

    Created spec for the toggle method.

--- tests/specs/jquery_checkboxes_check_spec.js
+++ tests/specs/jquery_checkboxes_check_spec.js
@@ -7,39 +7,39 @@ jasmine.getFixtures().fixturesPath = 'tests/fixtures';
 describe('The `check` method', function() {
 
     it('should exists', function() {
-        loadFixtures('all_unchecked.html');
+        loadFixtures('unchecked.html');
         var el = $('body').checkboxes();
         expect(el.data('checkboxes').check).toBeDefined();
     });
 
     it('should be a function', function() {
-        loadFixtures('all_unchecked.html');
+        loadFixtures('unchecked.html');
         var el = $('body').checkboxes();
         expect(typeof el.data('checkboxes').check).toBe('function');
     });
 
     it('should check all checkboxes in context', function() {
-        loadFixtures('all_unchecked.html');
+        loadFixtures('unchecked.html');
         var el = $('body').checkboxes('check');
         expect(el.find(':checkbox')).toBeChecked();
     });
 
     it('should check specified checkbox', function() {
         // Check first checkbox.
-        loadFixtures('all_unchecked.html');
+        loadFixtures('unchecked.html');
         var el = $(':checkbox:first').parent().checkboxes('check');
         expect(el.find(':checkbox:first')).toBeChecked();
         expect(el.find(':checkbox:not(:first)')).toBeChecked();
 
         // Check last checkbox.
-        loadFixtures('all_unchecked.html');
+        loadFixtures('unchecked.html');
         var el = $(':checkbox:last').parent().checkboxes('check');
         expect(el.find(':checkbox:last')).toBeChecked();
         expect(el.find(':checkbox:not(:last)')).toBeChecked();
     });
 
     it('should check all checkboxes in context, but no disabled ones', function() {
-        loadFixtures('all_unchecked.html');
+        loadFixtures('unchecked.html');
         var el = $('body').checkboxes('check');
         expect(el.find(':checkbox:disabled')).not.toBeChecked();
         expect(el.find(':checkbox:not(:disabled)')).toBeChecked();
--- tests/specs/jquery_checkboxes_uncheck_spec.js
+++ tests/specs/jquery_checkboxes_uncheck_spec.js
@@ -7,39 +7,39 @@ jasmine.getFixtures().fixturesPath = 'tests/fixtures';
 describe('The `uncheck` method', function() {
 
     it('should exists', function() {
-        loadFixtures('all_checked.html');
+        loadFixtures('checked.html');
         var el = $('body').checkboxes();
         expect(el.data('checkboxes').uncheck).toBeDefined();
     });
 
     it('should be a function', function() {
-        loadFixtures('all_checked.html');
+        loadFixtures('checked.html');
         var el = $('body').checkboxes();
         expect(typeof el.data('checkboxes').uncheck).toBe('function');
     });
 
     it('should uncheck all checkboxes in context', function() {
-        loadFixtures('all_checked.html');
+        loadFixtures('checked.html');
         var el = $('body').checkboxes('uncheck');
         expect(el.find(':checkbox:not(:disabled)')).not.toBeChecked();
     });
 
     it('should uncheck specified checkbox', function() {
         // Check first checkbox.
-        loadFixtures('all_checked.html');
+        loadFixtures('checked.html');
         var el = $(':checkbox:first').parent().checkboxes('uncheck');
         expect(el.find(':checkbox:first')).not.toBeChecked();
         expect(el.find(':checkbox:not(:first)')).toBeChecked();
 
         // Check last checkbox.
-        loadFixtures('all_checked.html');
+        loadFixtures('checked.html');
         var el = $(':checkbox:last').parent().checkboxes('uncheck');
         expect(el.find(':checkbox:last')).not.toBeChecked();
         expect(el.find(':checkbox:not(:last)')).toBeChecked();
     });
 
     it('should check all checkboxes in context, but no disabled ones', function() {
-        loadFixtures('all_checked.html');
+        loadFixtures('checked.html');
         var el = $('body').checkboxes('uncheck');
         expect(el.find(':checkbox:disabled')).toBeChecked();
         expect(el.find(':checkbox:not(:disabled)')).not.toBeChecked();
