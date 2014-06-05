9c37d39f730da08f005f6e1affca9d332566fd90
Rubens Mariuzzo <rubens@mariuzzo.com>
2014-06-04 21:14:07 -0400

    Created spec for the jQuery plugin and for the check method.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,43 +12,51 @@ module.exports = function(grunt) {
             scripts: {
                 files: [
                     'Gruntfile.js',
-                    'src/**.js',
-                    'tests/spec/**.js'
+                    'src/**/*.js',
+                    'tests/specs/**/*.js',
+                    '!.grunt'
                 ],
                 tasks: ['jshint', 'jasmine']
             }
         },
 
         uglify: {
-            build: {
-                src: 'src/jquery.checkboxes.js',
-                dest: 'dist/jquery.checkboxes-<%= pkg.version %>.min.js'
-            },
-            options: {
-                banner: '/*! checkboxes.js v<%= pkg.version %> | ' +
+            all: {
+                build: {
+                    src: 'src/jquery.checkboxes.js',
+                    dest: 'dist/jquery.checkboxes-<%= pkg.version %>.min.js'
+                },
+                options: {
+                    banner: '/*! checkboxes.js v<%= pkg.version %> | ' +
                         '(c) 2013, <%= grunt.template.today("yyyy") %> Rubens Mariuzzo | ' +
                         'http://github.com/rmariuzzo/checkboxes.js/LICENSE */',
+                }
             }
         },
 
         jshint: {
-            src: [
-                'Gruntfile.js',
-                'src/**.js',
-                'tests/spec/**.js'
-            ],
-            options: {
-                jshintrc: true
+            all: {
+                src: [
+                    'Gruntfile.js',
+                    'src/**/*.js',
+                    'tests/spec/**/*.js'
+                ],
+                options: {
+                    jshintrc: true
+                }
             }
         },
 
         jasmine: {
-            src: 'src/**.js',
-            options: {
-                specs: 'tests/spec/*_spec.js',
-                vendor: [
-                    'bower_components/jquery/dist/jquery.min.js'
-                ]
+            all: {
+                src: 'src/**/*.js',
+                options: {
+                    specs: 'tests/specs/*_spec.js',
+                    vendor: [
+                        'bower_components/jquery/dist/jquery.min.js',
+                        'bower_components/jasmine-jquery/lib/jasmine-jquery.js'
+                    ]
+                }
             }
         }
 
---
+++ tests/specs/jquery_checkboxes_spec.js
@@ -0,0 +1,29 @@
+/* globals $, jasmine, loadFixtures */
+
+'use strict';
+
+jasmine.getFixtures().fixturesPath = 'tests/fixtures';
+
+describe('The jQuery checkboxes plugin', function() {
+
+    it('should exists', function() {
+        expect($.fn.checkboxes).toBeDefined();
+    });
+
+    it('should be a function', function() {
+        expect(typeof $.fn.checkboxes).toBe('function');
+    });
+
+    it('should create an instance in $.data', function() {
+        loadFixtures('basic.html');
+        var el = $('body').checkboxes();
+        expect(el.data('checkboxes')).toBeDefined();
+    });
+
+    it('should not create an instance in $.data with empty element set', function() {
+        loadFixtures('basic.html');
+        var el = $().checkboxes();
+        expect(el.data('checkboxes')).not.toBeDefined();
+    });
+
+});
